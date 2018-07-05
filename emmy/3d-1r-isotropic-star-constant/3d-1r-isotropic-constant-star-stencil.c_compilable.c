19a31975aac235fd9b9ce071adb31c9c0906f004
sguera <sguera@users.noreply.github.com>
2018-07-05 14:21:02 +0200

    Update 3d-1r-isotropic-constant-star-stencil.c_compilable.c

--- emmy/3d-1r-isotropic-star-constant/3d-1r-isotropic-constant-star-stencil.c_compilable.c
+++ emmy/3d-1r-isotropic-star-constant/3d-1r-isotropic-constant-star-stencil.c_compilable.c
@@ -1,53 +1,124 @@
-#include "kerncraft.h"
 #include <stdlib.h>
+#include <math.h>
 
-void dummy(void *);
+#include "timing.h"
+#include "kerncraft.h"
+#include "kernel.c"
+#ifdef LIKWID_PERFMON
+#include <likwid.h>
+#endif
+#include "dim_input.h"
+
+void* aligned_malloc(size_t, size_t);
 extern int var_false;
+void dummy(double *);
+extern void kernel_loop(double *a, double *b, double c0, double c1, int N, int M, int P);
 int main(int argc, char **argv)
 {
-  const int N = atoi(argv[3]);
-  const int P = atoi(argv[2]);
-  const int M = atoi(argv[1]);
+  
+  #ifdef LIKWID_PERFMON
+  LIKWID_MARKER_INIT;
+  #endif
+
+  
+  #ifdef LIKWID_PERFMON
+  #pragma omp parallel
+  {
+    LIKWID_MARKER_THREADINIT;
+  }
+  #endif
+
+  int P = P_MAX;
+  int M = M_MAX;
+  int N = N_MAX;
   double *a = aligned_malloc((sizeof(double)) * ((M * N) * P), 32);
   for (int i = 0; i < ((M * N) * P); ++i)
-    a[i] = 0.49795506534730005;
+    a[i] = rand() / ((double ) RAND_MAX);
 
   double *b = aligned_malloc((sizeof(double)) * ((M * N) * P), 32);
   for (int i = 0; i < ((M * N) * P); ++i)
-    b[i] = 0.4150803324887108;
+    b[i] = rand() / ((double ) RAND_MAX);
 
   double c0;
-  c0 = 0.7607425694244915;
+  c0 = (rand() / ((double ) RAND_MAX)) / 4.0;
   double c1;
-  c1 = 0.5922194377496228;
-  if (var_false)
+  c1 = (rand() / ((double ) RAND_MAX)) / 4.0;
+  int repeat = 1;
+  double runtime = 0.0;
+  double wct_start;
+  double wct_end;
+  double cput_start;
+  double cput_end;
+  double *tmp;
+  
+  /* Warmup: 2 iterations*/;
+  for (int n = 0; n < 2; ++n)
   {
-    dummy(a);
-    dummy(b);
-    dummy(&c0);
-    dummy(&c1);
+    kernel_loop(a, b, c0, c1, N, M, P);
+    tmp = a;
+    a = b;
+    b = tmp;
   }
 
+  
+  #ifdef LIKWID_PERFMON
+  #pragma omp parallel
+  {
+    LIKWID_MARKER_START("Sweep");
+  }
+  #endif
+
+  while ((runtime < 2.0) || (repeat <= 2))
+  {
+    timing(&wct_start, &cput_start);
+    for (int n = 0; n < repeat; ++n)
+    {
+      kernel_loop(a, b, c0, c1, N, M, P);
+      tmp = a;
+      a = b;
+      b = tmp;
+    }
+
+    timing(&wct_end, &cput_end);
+    runtime = wct_end - wct_start;
+    repeat *= 2;
+  }
+
+  
+  #ifdef LIKWID_PERFMON
+  #pragma omp parallel
+  {
+    LIKWID_MARKER_STOP("Sweep");
+  }
+  #endif
+
+  repeat /= 2;
+  printf("iterations: %d\n", repeat);
+  printf("Total iterations: %lld LUP\n", ((long long ) repeat) * ((((long long ) (M - 2)) * ((long long ) (N - 2))) * ((long long ) (P - 2))));
+  printf("FLOP: 8\n");
+  printf("Total work: %lld FLOP\n", (((long long ) repeat) * ((((long long ) (M - 2)) * ((long long ) (N - 2))) * ((long long ) (P - 2)))) * 8);
+  printf("performance: %lf GLUP/s\n", ((((double ) repeat) * ((((double ) (M - 2)) * ((double ) (N - 2))) * ((double ) (P - 2)))) / runtime) / 1000000000.);
+  printf("Performance in GFLOP/s: %lf\n", (((((double ) repeat) * ((((double ) (M - 2)) * ((double ) (N - 2))) * ((double ) (P - 2)))) * 8) / runtime) / 1000000000.);
+  printf("size: %d\n", (N * M * P));
+  printf("time: %lf\n", runtime);
+  double total = 0.0;
   for (int k = 1; k < (M - 1); k++)
   {
     for (int j = 1; j < (N - 1); j++)
     {
       for (int i = 1; i < (P - 1); i++)
       {
-        b[(i + (j * P)) + (k * (P * N))] = (c0 * a[(i + (j * P)) + (k * (P * N))]) + (c1 * (((a[((i - 1) + (j * P)) + (k * (P * N))] + a[((i + 1) + (j * P)) + (k * (P * N))]) + (a[(i + (j * P)) + ((k - 1) * (P * N))] + a[(i + (j * P)) + ((k + 1) * (P * N))])) + (a[(i + ((j - 1) * P)) + (k * (P * N))] + a[(i + ((j + 1) * P)) + (k * (P * N))])));
+        total = total + (a[(i + (j * P)) + (k * (P * N))] - b[(i + (j * P)) + (k * (P * N))]);
       }
 
     }
 
   }
 
-  if (var_false)
-  {
-    dummy(a);
-    dummy(b);
-    dummy(&c0);
-    dummy(&c1);
-  }
+  printf("diff(a-b): %lf\n", total);
+  
+  #ifdef LIKWID_PERFMON
+  LIKWID_MARKER_CLOSE;
+  #endif
 
 }
-
