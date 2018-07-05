57757f056214c6abe513e89a563f25c7eaf4e34e
sguera <sguera@users.noreply.github.com>
2018-07-05 14:20:06 +0200

    Update 2d-3r-heterogeneous-variable-star-stencil.c_compilable.c

--- emmy/2d-3r-heterogeneous-star-variable/2d-3r-heterogeneous-variable-star-stencil.c_compilable.c
+++ emmy/2d-3r-heterogeneous-star-variable/2d-3r-heterogeneous-variable-star-stencil.c_compilable.c
@@ -1,46 +1,119 @@
-#include "kerncraft.h"
 #include <stdlib.h>
+#include <math.h>
 
-void dummy(void *);
+#include "timing.h"
+#include "kerncraft.h"
+#include "kernel.c"
+#ifdef LIKWID_PERFMON
+#include <likwid.h>
+#endif
+#include "dim_input.h"
+
+void* aligned_malloc(size_t, size_t);
 extern int var_false;
+void dummy(double *);
+extern void kernel_loop(double *a, double *b, double *W, int M, int N);
 int main(int argc, char **argv)
 {
-  const int M = atoi(argv[2]);
-  const int N = atoi(argv[1]);
+  
+  #ifdef LIKWID_PERFMON
+  LIKWID_MARKER_INIT;
+  #endif
+
+  
+  #ifdef LIKWID_PERFMON
+  #pragma omp parallel
+  {
+    LIKWID_MARKER_THREADINIT;
+  }
+  #endif
+
+  int N = N_MAX;
+  int M = M_MAX;
   double *a = aligned_malloc((sizeof(double)) * (M * N), 32);
   for (int i = 0; i < (M * N); ++i)
-    a[i] = 0.49795506534730005;
+    a[i] = rand() / ((double ) RAND_MAX);
 
   double *b = aligned_malloc((sizeof(double)) * (M * N), 32);
   for (int i = 0; i < (M * N); ++i)
-    b[i] = 0.4150803324887108;
+    b[i] = rand() / ((double ) RAND_MAX);
 
   double *W = aligned_malloc((sizeof(double)) * ((M * N) * 13), 32);
   for (int i = 0; i < ((M * N) * 13); ++i)
-    W[i] = 0.7607425694244915;
+    W[i] = (rand() / ((double ) RAND_MAX)) / 15.0;
 
-  if (var_false)
+  int repeat = 1;
+  double runtime = 0.0;
+  double wct_start;
+  double wct_end;
+  double cput_start;
+  double cput_end;
+  double *tmp;
+  
+  /* Warmup: 2 iterations*/;
+  for (int n = 0; n < 2; ++n)
   {
-    dummy(a);
-    dummy(b);
-    dummy(W);
+    kernel_loop(a, b, W, M, N);
+    tmp = a;
+    a = b;
+    b = tmp;
   }
 
+  
+  #ifdef LIKWID_PERFMON
+  #pragma omp parallel
+  {
+    LIKWID_MARKER_START("Sweep");
+  }
+  #endif
+
+  while ((runtime < 2.0) || (repeat <= 2))
+  {
+    timing(&wct_start, &cput_start);
+    for (int n = 0; n < repeat; ++n)
+    {
+      kernel_loop(a, b, W, M, N);
+      tmp = a;
+      a = b;
+      b = tmp;
+    }
+
+    timing(&wct_end, &cput_end);
+    runtime = wct_end - wct_start;
+    repeat *= 2;
+  }
+
+  
+  #ifdef LIKWID_PERFMON
+  #pragma omp parallel
+  {
+    LIKWID_MARKER_STOP("Sweep");
+  }
+  #endif
+
+  repeat /= 2;
+  printf("iterations: %d\n", repeat);
+  printf("Total iterations: %lld LUP\n", ((long long ) repeat) * (((long long ) (M - 6)) * ((long long ) (N - 6))));
+  printf("FLOP: 25\n");
+  printf("Total work: %lld FLOP\n", (((long long ) repeat) * (((long long ) (M - 6)) * ((long long ) (N - 6)))) * 25);
+  printf("performance: %lf GLUP/s\n", ((((double ) repeat) * (((double ) (M - 6)) * ((double ) (N - 6)))) / runtime) / 1000000000.);
+  printf("Performance in GFLOP/s: %lf\n", (((((double ) repeat) * (((double ) (M - 6)) * ((double ) (N - 6)))) * 25) / runtime) / 1000000000.);
+  printf("size: %d\n", (M * N));
+  printf("time: %lf\n", runtime);
+  double total = 0.0;
   for (int j = 3; j < (M - 3); j++)
   {
     for (int i = 3; i < (N - 3); i++)
     {
-      b[i + (j * N)] = ((((((((((((W[(0 + (i * 13)) + (j * (13 * N))] * a[i + (j * N)]) + (W[(1 + (i * 13)) + (j * (13 * N))] * a[(i - 1) + (j * N)])) + (W[(2 + (i * 13)) + (j * (13 * N))] * a[(i + 1) + (j * N)])) + (W[(3 + (i * 13)) + (j * (13 * N))] * a[i + ((j - 1) * N)])) + (W[(4 + (i * 13)) + (j * (13 * N))] * a[i + ((j + 1) * N)])) + (W[(5 + (i * 13)) + (j * (13 * N))] * a[(i - 2) + (j * N)])) + (W[(6 + (i * 13)) + (j * (13 * N))] * a[(i + 2) + (j * N)])) + (W[(7 + (i * 13)) + (j * (13 * N))] * a[i + ((j - 2) * N)])) + (W[(8 + (i * 13)) + (j * (13 * N))] * a[i + ((j + 2) * N)])) + (W[(9 + (i * 13)) + (j * (13 * N))] * a[(i - 3) + (j * N)])) + (W[(10 + (i * 13)) + (j * (13 * N))] * a[(i + 3) + (j * N)])) + (W[(11 + (i * 13)) + (j * (13 * N))] * a[i + ((j - 3) * N)])) + (W[(12 + (i * 13)) + (j * (13 * N))] * a[i + ((j + 3) * N)]);
+      total = total + (a[i + (j * N)] - b[i + (j * N)]);
     }
 
   }
 
-  if (var_false)
-  {
-    dummy(a);
-    dummy(b);
-    dummy(W);
-  }
+  printf("diff(a-b): %lf\n", total);
+  
+  #ifdef LIKWID_PERFMON
+  LIKWID_MARKER_CLOSE;
+  #endif
 
 }
-
